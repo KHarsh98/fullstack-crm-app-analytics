fd89d519d448c996b20272f65b5099313b7284e7
KHarshSJSU <kumar.harsh@sjsu.edu>
2024-11-11 19:33:22 -0800

    feat: updated backend

--- frontend/src/App.js
+++
@@ -1,8 +0,0 @@
-import "./App.css";
-
-function App() {
-  // TODO: Make this index page
-  return <div></div>;
-}
-
-export default App;
--- frontend/src/constants/ORDER_STATUS.js
+++
@@ -1,5 +0,0 @@
-const orderStatuses = {};
-orderStatuses.PENDING = 'Pending';
-orderStatuses.OUT_FOR_DELIVERY = 'Out for delivery';
-orderStatuses.DELIVERED = 'Delivered';
-export default orderStatuses;
